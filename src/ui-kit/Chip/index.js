85916fe919fcf0d04b36e147b168f5888f138e57
AhmedKambaev <kambaevahmed@list.ru>
2019-10-10 11:05:30 +0400

    deleted styles

--- src/ui-kit/Chip/index.js
+++ src/ui-kit/Chip/index.js
@@ -1,16 +1,13 @@
 // @flow
 import React, { type Node } from 'react';
 import classNames from 'classnames';
-import classes from './Chips.module.scss';
+import styles from './Chips.module.scss';
 
 type Props = {
   id: string,
   title: string,
   onDelete: (string) => void,
   className?: string,
-  styles?: {
-    [string]: number | string
-  }
 }
 
 const Chip = (restProps: Props): Node => {
@@ -19,15 +16,14 @@ const Chip = (restProps: Props): Node => {
     id,
     onDelete,
     className,
-    styles,
   } = restProps;
   return (
-    <span style={styles} className={classNames(classes.chip, className)} {...restProps}>
+    <span className={classNames(styles.chip, className)} {...restProps}>
       {title}
       <button
         type="button"
         onClick={() => onDelete(id)}
-        className={classes['close-button']}
+        className={styles['close-button']}
       >
         <i className="icon-cancel" />
       </button>
@@ -37,7 +33,6 @@ const Chip = (restProps: Props): Node => {
 
 Chip.defaultProps = {
   className: '',
-  styles: {},
 };
 
 export default Chip;
