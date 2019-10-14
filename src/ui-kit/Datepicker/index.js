2f984f99854488a5a5a078a39421c96c830c93fc
Bulat Gombotsyrenov <bgombotsyrenov@sfxdx.ru>
2019-10-14 15:54:17 +0700

    datepicker

--- src/ui-kit/Datepicker/index.js
+++ src/ui-kit/Datepicker/index.js
@@ -1,30 +1,84 @@
 // @flow
 import React from 'react';
 import DayPickerInput from 'react-day-picker/DayPickerInput';
+import { format as formatDate } from 'date-fns';
 // $FlowFixMe
 import 'react-day-picker/lib/style.css';
 
 import styles from './Datepicker.module.scss';
 import overlayStyles from './Overlay.module.scss';
 
+const DEFAULT_FORMAT = 'DD.MM.YYYY';
+
 type Props = {
   onDayChange: Date => void,
+  format?: string,
+  placeholder?: string
 };
+
+type NavbarElementProps = {
+  onNextClick: () => void,
+  onPreviousClick: () => void
+}
 
 const weekdaysShort = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
 
-const Datepicker = ({ onDayChange }: Props) => (
+const Navbar = ({ onNextClick, onPreviousClick }: NavbarElementProps) => (
+  <div className={overlayStyles.nav}>
+    <button
+      type="button"
+      onClick={() => onPreviousClick()}
+    >
+      <i className="icon-left-open" />
+    </button>
+    <button
+      type="button"
+      onClick={() => onNextClick()}
+    >
+      <i className="icon-right-open" />
+    </button>
+  </div>
+);
+
+const DateInput = (props) => (
+  <div
+    className={styles.input}
+  >
+    <input {...props} />
+    <span className={styles.icon__wrap}>
+      <i className="icon-calendar" />
+    </span>
+  </div>
+);
+
+const getDate = (date: Date, format: string) => formatDate(date, format);
+
+const Datepicker = ({
+  onDayChange,
+  format = DEFAULT_FORMAT,
+  placeholder = DEFAULT_FORMAT,
+}: Props) => (
   <div className={styles.datepicker}>
     <DayPickerInput
       dayPickerProps={{
         classNames: overlayStyles,
         weekdaysShort,
+        navbarElement: Navbar,
       }}
+      component={DateInput}
+      classNames={styles}
+      format={format}
+      placeholder={placeholder}
+      formatDate={getDate}
       onDayChange={onDayChange}
-      showOverlay
     />
   </div>
 );
+
+Datepicker.defaultProps = {
+  format: DEFAULT_FORMAT,
+  placeholder: DEFAULT_FORMAT,
+};
 
 export default Datepicker;
