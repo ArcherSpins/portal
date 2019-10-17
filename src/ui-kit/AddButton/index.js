7443392718ea23a895c0ef3e117b5a9512f0536b
Alexander Rybin <arybin@sfxdx.ru>
2019-10-17 17:22:50 +1000

    add spinner disabled popover

--- src/ui-kit/AddButton/index.js
+++ src/ui-kit/AddButton/index.js
@@ -1,18 +1,22 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable linebreak-style */
 /* eslint-disable react/button-has-type */
 // @flow
 import React from 'react';
 import styles from './AddButton.module.scss';
+import Spinner from '../Spinner';
 
 type Props = {
     type?: string,
-    className: string
+    className: string,
+    loading: boolean
 }
 
 
-const AddButton = ({ type, className }: Props) => (
-  <button type={type} className={styles[className]}>
-    <div>Add Task</div>
+const AddButton = ({ type, className, loading = true }: Props) => (
+  <button type={type} className={styles[className]} disabled={false}>
+    {!loading && (<div className={styles.popover}>Add Task</div>)}
+    {loading ? <Spinner data-type="spinner" /> : <span>+</span>}
   </button>
 );
 
