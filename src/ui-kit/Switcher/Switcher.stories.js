a4b05ea80dd0457c18264c5f2b5a998e886a3ddc
Bulat Gombotsyrenov <bgombotsyrenov@sfxdx.ru>
2019-10-17 15:34:11 +0700

    stories add

--- src/ui-kit/Switcher/Switcher.stories.js
+++ src/ui-kit/Switcher/Switcher.stories.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react';
 import Switcher from '.';
@@ -8,6 +8,28 @@ export default {
 };
 
 const items = ['one', 'two', 'three'];
+const anotherItems = [
+  {
+    label: 'one',
+    value: '#1',
+  },
+  {
+    label: 'two',
+    value: '#2',
+  },
+  {
+    label: 'three',
+    value: '#3',
+  },
+];
+
+const Wrap = () => {
+  const [value, setValue] = useState('');
+  return (
+    <Switcher value={value} items={anotherItems} onChange={setValue} />
+  );
+};
+
 
 storiesOf('Switcher', module)
   .addWithChapters('Switcher', {
@@ -24,6 +46,12 @@ storiesOf('Switcher', module)
             title: 'Small Switcher',
             sectionFn: () => (
               <Switcher size="sm" value="one" items={items} onChange={action('clicked')} />
+            ),
+          },
+          {
+            title: 'Toggle switcher',
+            sectionFn: () => (
+              <Wrap />
             ),
           },
         ],
