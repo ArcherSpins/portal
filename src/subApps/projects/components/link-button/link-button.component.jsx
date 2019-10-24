bdb780ba3e61af3fc2feebdd2a94f61ad30627a4
Bulat Gombotsyrenov <bgombotsyrenov@sfxdx.ru>
2019-10-24 16:06:51 +0700

    fix link

--- src/subApps/projects/components/link-button/link-button.component.jsx
+++ src/subApps/projects/components/link-button/link-button.component.jsx
@@ -1,18 +1,15 @@
 // @flow
-import React, { type Node } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import { Button } from 'ui-kit';
+
 import './link-button.styles.scss';
 
 type Props = {
-  to: string,
-  children: Node
+  to: string
 };
 
-const LinkButton = ({ to, children, ...rest }: Props) => (
-  <Button className="link-button" {...rest}>
-    <Link to={to}>{children}</Link>
-  </Button>
+const LinkButton = (props: Props) => (
+  <Link className="link-button" {...props} />
 );
 
 
