444dfea0a4fa97e2351b88929c09c18d8ae1faf9
Alexander Rybin <arybin@sfxdx.ru>
2019-10-28 16:10:22 +1000

    create link-button

---
+++ src/ui-kit/LinkButton/index.js
@@ -0,0 +1,45 @@
+// @flow
+import React, { type Node } from 'react';
+import { Link } from 'react-router-dom';
+import classNames from 'classnames';
+import noop from 'lodash.noop';
+import styles from './LinkButton.module.scss';
+
+type Props = {
+    to: string,
+    type?: string,
+    onClick?: (SyntheticMouseEvent<HTMLButtonElement>) => void,
+    className?: string,
+    children: Node,
+    disabled?: boolean
+
+}
+
+
+const LinkButton = ({
+  to,
+  type,
+  onClick,
+  className,
+  children,
+  disabled,
+}: Props) => (
+  <Link
+    to={to}
+    className={classNames(styles.linkButton, className, { [styles.disabledLinkButtongit]: disabled })}
+    disabled={disabled}
+    onClick={onClick}
+    type={type}
+  >
+    {children}
+  </Link>
+);
+
+LinkButton.defaultProps = {
+  onClick: noop,
+  type: '',
+  disabled: false,
+  className: '',
+};
+
+export default LinkButton;
