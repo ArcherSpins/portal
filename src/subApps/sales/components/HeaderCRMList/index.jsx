76f3417e2a144f7853489e7a2611b726bdca27b9
AhmedKambaev <kambaevahmed@list.ru>
2019-11-07 17:36:01 +0400

    end redesign

--- src/subApps/sales/components/HeaderCRMList/index.jsx
+++ src/subApps/sales/components/HeaderCRMList/index.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import './style.scss';
 
 const HeaderCRMList = () => (
-  <div className="header-crm-list fz-14">
-    <ul>
-      <li>
-        <div>Lead</div>
-      </li>
-      <li>
-        <div>Introduction</div>
-      </li>
-      <li>
-        <div>Estimation</div>
-      </li>
-      <li>
-        <div>Negotiation</div>
-      </li>
-    </ul>
+  <div>
+    <div className="statuses-crm-list">
+      <p className="success_status">6 Tasks for Today</p>
+      <p className="error_status">12 overdue Tasks</p>
+      <p className="default_status">16 without Tasks</p>
+    </div>
+    <div className="header-crm-list fz-14">
+      <ul>
+        <li>
+          <div>Lead</div>
+        </li>
+        <li>
+          <div>Introduction</div>
+        </li>
+        <li>
+          <div>Estimation</div>
+        </li>
+        <li>
+          <div>Negotiation</div>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
