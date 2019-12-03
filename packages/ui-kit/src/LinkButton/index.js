921e160fe4ab3f69866fc96277a20388a6b65066
Bulat Gombotsyrenov <bgombotsyrenov@sfxdx.ru>
2019-12-03 11:30:40 +0700

    fix switcher

--- packages/ui-kit/src/LinkButton/index.js
+++ packages/ui-kit/src/LinkButton/index.js
@@ -3,7 +3,6 @@ import React, { type Node } from 'react';
 import { Link } from 'react-router-dom';
 import classNames from 'classnames';
 import { type ButtonSize } from '../Button';
-import { type ButtonUse } from '../Button/types';
 import styles from './LinkButton.module.scss';
 import buttonStyles from '../Button/Button.module.scss';
 
@@ -12,12 +11,11 @@ type Props = {
   children: Node,
   size?: ButtonSize,
   to: string,
-  use?: ButtonUse,
   className?: string
 }
 
 const LinkButton = ({
-  children, size, use, to, className, ...restProps
+  children, size, to, className, ...restProps
 }: Props) => (
   <Link
     {...restProps}
@@ -25,7 +23,6 @@ const LinkButton = ({
     className={classNames(
       styles.link,
       buttonStyles[size],
-      buttonStyles[use],
       className,
     )}
   >
@@ -35,7 +32,6 @@ const LinkButton = ({
 
 LinkButton.defaultProps = {
   size: 'md',
-  use: 'default',
   className: '',
 };
 
